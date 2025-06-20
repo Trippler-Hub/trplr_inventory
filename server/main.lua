aa441f919b84bedb8807f80be47a9f88635c6872
Lenix <149199196+LenixDev@users.noreply.github.com>
2025-06-20 20:57:20 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,16 @@
 if GetCurrentResourceName() ~= "trplr_inventory" then
     return print("^6Changing the resource's name wont't let the resource start, ^1" .. GetCurrentResourceName() .. "^0 > ^2 trplr_inventory ^7")
 end
+
+CreateThread(function()
+   print("  ____              _                      _        ")
+   print(" | __ )   _   _    | |       ___   _ __   (_) __  __")
+   print(" |  _ \\  | | | |   | |     / _ \\  | '_ \\  | | \\\\/ /")
+   print(" | |_) | | |_| |   | |___  | __/  | | | | | |  >  < ")
+   print(" |____/   \\__, |   |_____| \\___|  |_| |_| |_| /_/\\\\ ")
+   print("          |___/                                     ")
+)
+
 QBCore = exports['qb-core']:GetCoreObject()
 Inventories = {}
 Drops = {}
