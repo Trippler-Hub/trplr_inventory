1183d0b4bd4619e6fa1302b8e856abba29d78d40
Lenix <149199196+LenixDev@users.noreply.github.com>
2025-09-01 20:43:17 +0300

    #

--- server/main.lua
+++ server/main.lua
@@ -1,16 +1,3 @@
-CreateThread(function()
-   print("  ____              _                      _        ")
-   print(" | __ )   _   _    | |       ___   _ __   (_) __  __")
-   print(" |  _ \\  | | | |   | |     / _ \\  | '_ \\  | | \\\\/ /")
-   print(" | |_) | | |_| |   | |___  | __/  | | | | | |  >  < ")
-   print(" |____/   \\__, |   |_____| \\___|  |_| |_| |_| /_/\\\\ ")
-   print("          |___/                                     ")
-end)
-
-if GetCurrentResourceName() ~= "trplr_inventory" then
-    return print("^6Changing the resource's name wont't let the resource start, ^1" .. GetCurrentResourceName() .. "^0 > ^2 trplr_inventory ^7")
-end
-
 QBCore = exports['qb-core']:GetCoreObject()
 Inventories = {}
 Drops = {}
